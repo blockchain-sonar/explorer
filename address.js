14b04469f86fdf6261217be9e209894b9e271443
Dmitry Detukov <dmitry.detukov@zxteam.net>
2022-07-26 22:00:15 +0300

    #22 Implement GET requests in address

--- address.js
+++ address.js
@@ -1,9 +1,17 @@
-window.onload = function () {
-	console.log("Hello, World 2 !!!");
+// http://127.0.0.1:5000/webapp/address/0xd3cda913deb6f67967b99d67acdfa1712c293601
 
-	var data = 20;
-	buildUI(data);
-}
+window.onload = function () {
+	const xhr = new XMLHttpRequest();
+	xhr.open('GET', 'http://127.0.0.1:5000/webapp/address/0xd3cda913deb6f67967b99d67acdfa1712c293601', true);
+	//xhr.responseType = 'json';
+	xhr.send();
+	xhr.onload = function() {
+		let responseObj = xhr.response;
+		console.log(responseObj); 
+	  };
+	  var data = 20;
+	  buildUI(data);
+  }
 
 function buildUI(data) {
 	for (var i = 0; i < data; ++i) {
