19f6683aaf143ca9d852e84822ed650c7136424a
Max Anurin <maxim.anurin@zxteam.net>
2022-07-26 10:36:34 +0300

    #22 Add few samples

--- address.js
+++ address.js
@@ -1,3 +1,14 @@
-window.onload=function() {
-	alert("Hello, World 2 !!!");
+window.onload = function () {
+	console.log("Hello, World 2 !!!");
+
+	var data = 20;
+	buildUI(data);
+}
+
+function buildUI(data) {
+	for (var i = 0; i < data; ++i) {
+		var p = document.createElement("p");
+		p.innerText = "Asset " + i;
+		document.getElementById("content").appendChild(p);
+	}
 }
