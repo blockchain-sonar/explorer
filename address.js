e7c7a1cf0df1f4d7415d361a78003ac8b9d58b5f
Dmitry Detukov <dmitry.detukov@zxteam.net>
2022-07-27 20:23:12 +0300

    #22 Implement backend request in address

--- address.js
+++ address.js
@@ -2,13 +2,24 @@
 // http://127.0.0.1:5000/explorer/v1/address/0xd3cda913deb6f67967b99d67acdfa1712c293601
 
 window.onload = function () {
+	var url_frontend = (decodeURI(window.location.href));
+	alert(url_frontend);
+
+	var urlsplit = url_frontend.split("/");
+	var url_split = urlsplit[5];
+	alert(typeof url_split)
+	
+	var url_backend = 'http://127.0.0.1:5000/explorer/v1/address/%s', url_split;
+	alert(url_backend);
+
 	const xhr = new XMLHttpRequest();
-	xhr.open('GET', 'http://127.0.0.1:5000/webapp/address/%s', true);
+	xhr.open('GET', url_backend, true);
 	//xhr.responseType = 'json';
 	xhr.send();
 	xhr.onload = function() {
 		let responseObj = xhr.response;
 		console.log(responseObj); 
+		
 	  };
 	  var data = 20;
 	  buildUI(data);
