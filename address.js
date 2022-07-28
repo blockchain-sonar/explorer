96a3dc4c3a37cf899a45fcf810d1ff79837650fc
Dmitry Detukov <dmitry.detukov@zxteam.net>
2022-07-28 15:26:58 +0300

    #22 Implement function buildUI

--- address.js
+++ address.js
@@ -5,27 +5,19 @@ window.onload = function () {
 	var url_frontend = (decodeURI(window.location.href));
 	var urlsplit = url_frontend.split("/");
 	var url_split = urlsplit[5];
-
 	var url_backend = 'http://127.0.0.1:5000/explorer/v1/address/' + url_split;
 
 	const xhr = new XMLHttpRequest();
 	xhr.open('GET', url_backend, true);
-	//xhr.responseType = 'json';
 	xhr.send();
 	xhr.onload = function() {
 		var responseObj = xhr.response;
-		
-		console.log(responseObj); 
-		var p = document.createElement("p");
-		p.innerHTML = responseObj;
-		document.getElementById("content").appendChild(p);
-		
+		buildUI(responseObj);
 	  };
-	 
   }
 
-
-		
-	
-
-  
+  function buildUI(responseObj) {
+		var p = document.createElement("p");
+		p.innerText = responseObj;
+		document.getElementById("content").appendChild(p);
+  }
