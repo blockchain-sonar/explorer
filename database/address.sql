904d05ae252499aa0454c95860105433c33f4ab9
Dmitry Detukov <dmitry.detukov@zxteam.net>
2023-05-09 17:36:14 +0300

    Added constraint to address

--- database/address.sql
+++ database/address.sql
@@ -1,7 +1,9 @@
 CREATE SEQUENCE "address_id_seq" START 1;
 
 CREATE TABLE "address" (
-    "id"        BIGINT PRIMARY KEY  NOT NULL    DEFAULT NEXTVAL('address_id_seq'),
-    "address"   BYTEA               NOT NULL,
-    "balance"   NUMERIC(48,24)
+    "id"                BIGINT PRIMARY KEY  NOT NULL    DEFAULT NEXTVAL('address_id_seq'),
+    "address"           BYTEA               NOT NULL,
+    "balance"           NUMERIC(48,24),
+    "transaction_id"    BIGINT              NOT NULL,
+    CONSTRAINT "fk__address__block" FOREIGN KEY ("transaction_id") REFERENCES "transaction" ("id")
 );
--- database/transaction.sql
+++ database/transaction.sql
@@ -4,6 +4,6 @@ CREATE TABLE "transaction" (
     "id"        BIGINT PRIMARY KEY  NOT NULL    DEFAULT NEXTVAL('transaction_id_seq'),
     "hash"      BYTEA               NOT NULL,
     "amount"    NUMERIC(48,24),
-    "block_id" BIGINT               NOT NULL,
+    "block_id"  BIGINT               NOT NULL,
     CONSTRAINT "fk__transaction__block" FOREIGN KEY ("block_id") REFERENCES "block" ("id")
 );
