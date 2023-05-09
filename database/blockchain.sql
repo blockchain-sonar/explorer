0d464bcb61ccb40707c4a8c11ce9e12e564c0e53
Dmitry Detukov <dmitry.detukov@zxteam.net>
2023-05-09 17:50:25 +0300

    Added foreign_key to blockchain

--- database/blockchain.sql
+++ database/blockchain.sql
@@ -2,6 +2,8 @@ CREATE SEQUENCE "blockchain_id_seq" START 1;
 
 CREATE TABLE "blockchain" (
     "id"    BIGINT PRIMARY KEY      NOT NULL    DEFAULT NEXTVAL('blockchain_id_seq'),
-    "name"  CHARACTER VARYING(100)  NOT NULL
+    "name"  CHARACTER VARYING(100)  NOT NULL,
+    "explorer_id"  BIGINT           NOT NULL,
+    CONSTRAINT "fk__explorer__block" FOREIGN KEY ("explorer_id") REFERENCES "explorer" ("id")
 );
 
